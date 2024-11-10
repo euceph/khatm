47648d80b8257ddf8074177afa79b2399522bf61
issa euceph <ieuceph@gmail.com>
2024-11-10 14:21:20 -0800

    Initial Commit

---
+++ khatm/khatmApp.swift
@@ -0,0 +1,17 @@
+//
+//  khatmApp.swift
+//  khatm
+//
+//  Created by issa euceph on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct khatmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ khatmTests/khatmTests.swift
@@ -0,0 +1,16 @@
+//
+//  khatmTests.swift
+//  khatmTests
+//
+//  Created by issa euceph on 11/10/24.
+//
+
+import Testing
+
+struct khatmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
